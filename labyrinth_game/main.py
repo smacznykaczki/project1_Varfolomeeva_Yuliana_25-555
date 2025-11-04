52a7ba6b57bf90a243f065c4d13a530764c3ffd8
Yuliana Varfolomeeva <yulianaw@yandex.ru>
2025-11-04 17:16:56 +0000

    Формирование архитектуры и данных

--- labyrinth_game/main.py
+++ labyrinth_game/main.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python3
 
+# Импорт модулей
+
+import constants
+import player_actions
+import utils
+
+# Импорт переменных
+
+from constants import ROOMS
+
+#Состояние игрока
+
+game_state = {
+    'player_inventory': [], # Инвентарь игрока
+    'current_room': 'entrance', # Текущая комната
+    'game_over': False, # Значения окончания игры
+    'steps_taken': 0 # Количество шагов
+  }
+
 def main():
     print("Первая попытка запустить проект!")
     
