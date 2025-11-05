216844b16e6fdb1472d560bc705f19ca3258cc65
Yuliana Varfolomeeva <yulianaw@yandex.ru>
2025-11-05 16:34:18 +0000

    Создан основной игровой цикл

--- labyrinth_game/main.py
+++ labyrinth_game/main.py
@@ -6,9 +6,11 @@ import constants
 import player_actions
 import utils
 
-# Импорт переменных
+# Импорт переменных и функций
 
 from constants import ROOMS
+from utils import describe_current_room
+from player_actions import get_input
 
 #Состояние игрока
 
@@ -20,7 +22,29 @@ game_state = {
   }
 
 def main():
-    print("Первая попытка запустить проект!")
+    """
+    Основная функция игры - игровой цикл
+    """
+    # Состояние игры
+    game_state = {
+        'player_inventory': [],      # Инвентарь игрока
+        'current_room': 'entrance',  # Текущая комната
+        'game_over': False,          # Флаг окончания игры
+        'steps_taken': 0             # Количество шагов
+    }
+    
+    # Приветственное сообщение
+    print("=== ДОБРО ПОЖАЛОВАТЬ В ЛАБИРИНТ СОКРОВИЩ! ===")
+    print("-" * 50)
+    
+    # Описываем стартовую комнату
+    describe_current_room(game_state)
+    
+    # Основной игровой цикл
+    while not game_state['game_over']:
+        
+        # Считываем команду от пользователя
+        user_input = get_input("Введите команду: ").strip().lower()
     
 if __name__ == "__main__":
     main()
